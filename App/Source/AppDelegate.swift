e997cd7a8cc4dc690cc8cc4aaacea03928b90fc5
Kieran Graham <me@kierangraham.com>
2018-10-11 13:28:00 +0200

    More manageable project structure

---
+++ App/Source/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  RxAPI
+//
+//  Created by Kieran Graham on 11/10/2018.
+//  Copyright © 2018 Kieran Graham. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+}
+
